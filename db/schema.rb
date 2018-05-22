9c18e50438dcd1835c32382ce07ebd4b36f3877a
Erik Djamgarian <djamgarian0702@gmail.com>
2018-05-22 18:02:40 +0100

    cars controller finito, neearly done with Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_151910) do
+ActiveRecord::Schema.define(version: 2018_05_22_162729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2018_05_22_151910) do
     t.boolean "booking_confirmation", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "car_id"
+    t.bigint "user_id"
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "cars", force: :cascade do |t|
@@ -60,4 +64,6 @@ ActiveRecord::Schema.define(version: 2018_05_22_151910) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
 end
