c501aa2c0f4a11a4b23b52edc2147bafb290dccc
Erik Djamgarian <djamgarian0702@gmail.com>
2018-05-22 14:45:09 +0100

    added gems, created dummy cars and added a form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,43 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def edit
+  end
+
+  def create
+    @car = Car.new(car_params)
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to car_path(@car)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @car.destroy
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:make, :rental_rate, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.last
+
+cars1 = Car.new(make: 'Ford mondeo', rental_rate: 2500, description: "Cool car", user: user )
+cars2 = Car.new(make: 'Opel astra', rental_rate: 2000, description: "Really cool car", user: user)
+
+
+cars1.save!
+cars2.save!
