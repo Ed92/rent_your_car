d81c367c1dd3fa99cf9b02384803b4ed9355faf7
Sebastian Luksic James <sebastian.luksic1@gmail.com>
2018-05-25 13:50:54 +0100

    bookings to profile page; also cards in profile page now link to show page (bookings); favico.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,7 @@ class BookingsController < ApplicationController
     @booking.price = @car.rental_rate * (params[:booking]["end_date(3i)"].to_i  - params[:booking]["start_date(3i)"].to_i)
     authorize @booking
     if @booking.save
-      redirect_to car_booking_path(@car, @booking)
+      redirect_to profile_path(current_user)
     end
   end
 
